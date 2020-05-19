b68c6c11aaf17feb766c44d8e67ea951f898d08f
QuietClickCode <2572417548@qq.com>
2020-05-19 16:39:41 +0800

    zjj的极简产品哲学3

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -28,7 +28,7 @@ font-size: 30px;"
         <el-avatar  type="primary" style="margin-left: 16px;" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
         </div>
       </div>
-      <div style="" :class="searchinput3" >
+      <div :class="searchinput3" >
         <div class="table-div section">
           <section class="centertest" >
             <div style="list-style:none;">
