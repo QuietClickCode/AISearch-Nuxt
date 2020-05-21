e59382166aa74671bf957a71b1be20cc1935c21b
QuietClickCode <2572417548@qq.com>
2020-05-21 15:35:28 +0800

    搞一个这是你第几次登录,第几次查询,本月,本年,本周的一个报告,可视化报告

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -38,30 +38,11 @@
     },
     mounted() {
       var ip = returnCitySN["cip"];
-
       console.log(ip, "ip");
-      getYourIP();
     },
 
 
     methods: {
-      getUserIP(){
-        var RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
-        if (RTCPeerConnection) (()=>{
-          var rtc = new RTCPeerConnection()
-          rtc.createDataChannel(''); //创建一个可以发送任意数据的数据通道
-        rtc.createOffer( offerDesc => { //创建并存储一个sdp数据
-          rtc.setLocalDescription(offerDesc)
-      }, e => { console.log(e)})
-        rtc.onicecandidate =(evt) => { //监听candidate事件
-          if (evt.candidate) {
-            var ip_addr = evt.candidate.address
-            localStorage.setItem('ip_addr',ip_addr)
-            console.log(ip_addr)
-          }}
-      })()
-      else{console.log("目前仅测试了chrome浏览器OK")}
-      },
       login() {
         var vueThis = this
         axios({
@@ -96,58 +77,6 @@
   }
 
 
-  function getYourIP(){
-    var RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
-    if (RTCPeerConnection) (function () {
-      var rtc = new RTCPeerConnection({iceServers:[]});
-      if (1 || window.mozRTCPeerConnection) {
-        rtc.createDataChannel('', {reliable:false});
-      };
-
-      rtc.onicecandidate = function (evt) {
-        if (evt.candidate) grepSDP("a="+evt.candidate.candidate);
-      };
-      rtc.createOffer(function (offerDesc) {
-        grepSDP(offerDesc.sdp);
-        rtc.setLocalDescription(offerDesc);
-      }, function (e) { console.warn("offer failed", e); });
-
-      var addrs = Object.create(null);
-      addrs["0.0.0.0"] = false;
-      function updateDisplay(newAddr) {
-        if (newAddr in addrs) return;
-        else addrs[newAddr] = true;
-        var displayAddrs = Object.keys(addrs).filter(function (k) { return addrs[k]; });
-        for(var i = 0; i < displayAddrs.length; i++){
-          if(displayAddrs[i].length > 16){
-            displayAddrs.splice(i, 1);
-            i--;
-          }
-        }
-        alert(displayAddrs[0])
-        document.getElementById('list').textContent = displayAddrs[0];
-      }
-
-      function grepSDP(sdp) {
-        var hosts = [];
-        sdp.split('\r\n').forEach(function (line, index, arr) {
-          if (~line.indexOf("a=candidate")) {
-            var parts = line.split(' '),
-              addr = parts[4],
-              type = parts[7];
-            if (type === 'host') updateDisplay(addr);
-          } else if (~line.indexOf("c=")) {
-            var parts = line.split(' '),
-              addr = parts[2];
-            updateDisplay(addr);
-          }
-        });
-      }
-    })();
-    else{
-      document.getElementById('list').textContent = "请使用主流浏览器：chrome,firefox,opera,safari";
-    }
-  }
 </script>
 <style>
 </style>
